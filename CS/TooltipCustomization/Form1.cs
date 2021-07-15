66cfd32b66f339bf175b87c6d749aed330e6b60c
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-07-15 12:43:33 +0300

    update the example

--- CS/TooltipCustomization/Form1.cs
+++ CS/TooltipCustomization/Form1.cs
@@ -2,14 +2,45 @@
 using System;
 using System.Windows.Forms;
 using DevExpress.Utils;
+using System.Collections.Generic;
+using System.Data;
 
 namespace TooltipCustomization {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
             InitializeComponent();
         }
+        // Create a data source.  
+        public class TestChartData
+        {
+            public string Argument { get; set; }
+            public double Value { get; set; }
+            public string Comment { get; set; }
+            public TestChartData(string arguments, double value, string comment)
+            {
+                Argument = arguments;
+                Value = value;
+                Comment = comment;
+            }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        }
+        private List<TestChartData> CreateChartData()
+        {
+            var list = new List<TestChartData>();
+            list.Add(new TestChartData("A", 4, "comment for A"));
+            list.Add(new TestChartData("B", 2, "comment for B"));
+            list.Add(new TestChartData("C", 17, "comment for C"));
+            list.Add(new TestChartData("D", 4, "comment for D"));
+            list.Add(new TestChartData("E", 17, "comment for E"));
+            list.Add(new TestChartData("F", 12, "comment for F"));
+            list.Add(new TestChartData("G", 15, "comment for G"));
+            return list;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
             // Create an empty chart.
             ChartControl chartControl = new ChartControl();
 
@@ -18,25 +49,47 @@ namespace TooltipCustomization {
             this.Controls.Add(chartControl);
 
             // Create a series and add points to it.
-            Series series1 = new Series("Series 1", ViewType.Bar);
-            series1.Points.Add(new SeriesPoint("A", new double[] { 4 }));
-            series1.Points.Add(new SeriesPoint("B", new double[] { 2 }));
-            series1.Points.Add(new SeriesPoint("C", new double[] { 17 }));
-            series1.Points.Add(new SeriesPoint("D", new double[] { 4 }));
-            series1.Points.Add(new SeriesPoint("E", new double[] { 17 }));
-            series1.Points.Add(new SeriesPoint("F", new double[] { 12 }));
-            series1.Points.Add(new SeriesPoint("G", new double[] { 15 }));
+            Series boundSeries = new Series("Bound Series", ViewType.Line);
+            // Assign the created data source to the series.
+            boundSeries.DataSource = CreateChartData();
+            boundSeries.ArgumentScaleType = ScaleType.Auto;
+            boundSeries.ArgumentDataMember = "Argument";
+            boundSeries.ValueScaleType = ScaleType.Numerical;
+            boundSeries.ValueDataMembers.AddRange(new string[] { "Value" });
+
+            Series unboundSeries = new Series("Unbound Series", ViewType.Line);
+            unboundSeries.Points.Add(new SeriesPoint("A", new double[] { 9 }));
+            unboundSeries.Points.Add(new SeriesPoint("B", new double[] { 7 }));
+            unboundSeries.Points.Add(new SeriesPoint("C", new double[] { 23 }));
+            unboundSeries.Points.Add(new SeriesPoint("D", new double[] { 9 }));
+            unboundSeries.Points.Add(new SeriesPoint("E", new double[] { 23 }));
+            unboundSeries.Points.Add(new SeriesPoint("F", new double[] { 17 }));
+            unboundSeries.Points.Add(new SeriesPoint("G", new double[] { 20 }));
+
+
+            Series unboundSeriesWithTag = new Series("Unbound Series with Tag", ViewType.Line);
+            unboundSeriesWithTag.Points.Add(new SeriesPoint("A", 2) { Tag = new { Test = "TestValue" } });
+            unboundSeriesWithTag.Points.Add(new SeriesPoint("B", 0) { Tag = new { Test = "TestValue" } });
+            unboundSeriesWithTag.Points.Add(new SeriesPoint("C", 15) { Tag = new { Test = "TestValue" } });
+            unboundSeriesWithTag.Points.Add(new SeriesPoint("D", 2) { Tag = new { Test = "TestValue" } });
+            unboundSeriesWithTag.Points.Add(new SeriesPoint("E", 15) { Tag = new { Test = "TestValue" } });
+            unboundSeriesWithTag.Points.Add(new SeriesPoint("F", 10) { Tag = new { Test = "TestValue" } });
+            unboundSeriesWithTag.Points.Add(new SeriesPoint("G", 13) { Tag = new { Test = "TestValue" } });
+
 
             // Add the series to the chart.
-            chartControl.Series.Add(series1);
-
+            chartControl.Series.AddRange(unboundSeries, unboundSeriesWithTag, boundSeries);
+            // Enable data point markers.
+            ((LineSeriesView)unboundSeries.View).MarkerVisibility = DefaultBoolean.True;
+            ((LineSeriesView)unboundSeriesWithTag.View).MarkerVisibility = DefaultBoolean.True;
+            ((LineSeriesView)boundSeries.View).MarkerVisibility = DefaultBoolean.True;
             // Disable a crosshair cursor.
             chartControl.CrosshairEnabled = DefaultBoolean.False;
 
             // Enable chart tooltips. 
             chartControl.ToolTipEnabled = DefaultBoolean.True;
 
-            // Show a tooltip's beak
+            // Show a tooltip's beak.
             ToolTipController controller = new ToolTipController();
             chartControl.ToolTipController = controller;
             controller.ShowBeak = true;
@@ -50,7 +103,9 @@ namespace TooltipCustomization {
             relativePosition.OffsetY = 2;
 
             // Specify the tooltip point pattern.
-            series1.ToolTipPointPattern = "Bar Series: {A}:{V}";
+            unboundSeries.ToolTipPointPattern = "Line Series: {A}:{V}";
+            unboundSeriesWithTag.ToolTipPointPattern = "{A}: {V} ({Test})";
+            boundSeries.ToolTipPointPattern = "{A}: {V} ({Comment})";
         }
     }
 }
--- Readme.md
+++ Readme.md
@@ -6,8 +6,41 @@
 # How to show and customize chart tooltips
 
 
-This example demonstrates how to enable and customize chart tooltips at runtime. <br /><br />To enable tooltips, set the <a href="https://documentation.devexpress.com/WindowsForms/DevExpressXtraChartsChartControl_ToolTipEnabledtopic.aspx">ChartControl.ToolTipEnabled</a> property to <strong>true</strong>.<br /><br />To learn more on chart tooltips, see the <a href="https://documentation.devexpress.com/#WindowsForms/CustomDocument11977">Tooltips</a> topic.<br /><br />
+This example demonstrates how to enable and customize chart tooltips at runtime. 
 
-<br/>
+To enable tooltips, set the [ChartControl.ToolTipEnabled](https://docs.devexpress.com/WindowsForms/DevExpress.XtraCharts.ChartControl.ToolTipEnabled) `true`. 
+
+Specify the tooltip display text in the [ToolTipPointPattern](https://docs.devexpress.com/CoreLibraries/DevExpress.XtraCharts.SeriesBase.ToolTipPointPattern) property.
+
+## Example Structure
+
+The following example consists of tree series with the specified tooltip patterns:
+
+*  unboundSeries - a simple unbound series.
+   Shows tooltips with the following pattern:
+
+    `Line Series: {A}:{V}`
+
+    ![unbound series](Images/unboundseries.png)
+
+* unboundSeriesWithTag - an unbound series with the specified [SeriesPoint.Tag](https://docs.devexpress.com/CoreLibraries/DevExpress.XtraCharts.SeriesPoint.Tag) property for each data point.
+    Shows tooltips with the pattern that references the `Tag` values:
+
+    `{A}: {V} ({Test})`
+
+    ![unbound series with tag](Images/unboundseries-with-tag.png)
+
+* boundSeries - a series that is bound to the data source.
+
+    Shows tooltips with the pattern that references the data source field values:
+
+    `{A}: {V} ({Comment})`
+
+    ![bound series](Images/boundseries.png)
+
+## Documentation 
+
+[Tooltip](https://docs.devexpress.com/WindowsForms/11977/controls-and-libraries/chart-control/end-user-features/tooltip-and-crosshair-cursor/tooltip)
+
 
 
